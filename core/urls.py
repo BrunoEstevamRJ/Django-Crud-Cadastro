97565a04523ed611c7215547882cb256971e03b6
Bruno Estevam <67297226+BrunoEstevamRJ@users.noreply.github.com>
2020-11-06 23:58:32 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from .views import form_manual
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', form_manual, name='core_form_manual'),    
+    path('', form_manual, name='core_form_manual'),   
+    path('arquivo/', form_manual, name='core_arquivo'),
 ]
